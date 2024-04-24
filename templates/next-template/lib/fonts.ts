08eb3759b967fba16d8d7c8018bacb1590b9c18d
abitocodes <hancom114@gmail.com>
2024-04-25 01:15:50 +0900

    fix: declared font type

--- templates/next-template/lib/fonts.ts
+++ templates/next-template/lib/fonts.ts
@@ -2,13 +2,14 @@
 import { JetBrains_Mono as FontMono } from "next/font/google"
 // import { GeistMono } from "geist/font/mono"
 import { GeistSans } from "geist/font/sans"
-
+import { NextFontWithVariable } from "next/dist/compiled/@next/font";
 // export const fontSans = FontSans({
 //   subsets: ["latin"],
 //   variable: "--font-sans",
 // })
+export const fontSans: NextFontWithVariable = GeistSans;
 
-export const fontSans = GeistSans
+// export const fontSans = GeistSans
 
 export const fontMono = FontMono({
   subsets: ["latin"],
