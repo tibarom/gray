46129db420a45be8c0cd5f01f10ee6a4165fb7c9
abitocodes <hancom114@gmail.com>
2024-04-29 10:53:37 +0900

    fix: fixed wrong words

--- templates/next-template/components/command-menu.tsx
+++ templates/next-template/components/command-menu.tsx
@@ -12,7 +12,7 @@ import {
 } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 
-import { docsConfig } from "@/config/abouts"
+import { aboutsConfig } from "@/config/abouts"
 import { cn } from "@/lib/utils"
 import { Button } from "@/registry/new-york/ui/button"
 import {
@@ -77,7 +77,7 @@ export function CommandMenu({ ...props }: DialogProps) {
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup heading="Links">
-            {docsConfig.mainNav
+            {aboutsConfig.mainNav
               .filter((navitem) => !navitem.external)
               .map((navItem) => (
                 <CommandItem
@@ -92,7 +92,7 @@ export function CommandMenu({ ...props }: DialogProps) {
                 </CommandItem>
               ))}
           </CommandGroup>
-          {docsConfig.sidebarNav.map((group) => (
+          {aboutsConfig.sidebarNav.map((group) => (
             <CommandGroup key={group.title} heading={group.title}>
               {group.items.map((navItem) => (
                 <CommandItem
